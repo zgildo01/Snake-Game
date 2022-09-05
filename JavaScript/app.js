b340f6b5bf92741dda11aaac387c422239b30bf7
zgildo01 <zzaragozagildo@gmail.com>
2022-09-04 20:57:04 -0400

    add more JS constant variables

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,12 @@
 const gameBoard = document.querySelector("#game-board");
 const context = gameBoard.getContext("2d");
 const scoreText = document .querySelector("#score-text");
+const resetBtn = document.querySelector('#reset-button');
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = 'purple';
+const snakeColor = 'yellow';
+const snakeBorder = 'black';
+const foodColor = 'red';
+const unitSize = 25;
+
