a4acac7313d67e971bcc238bdc6b209657e07622
zgildo01 <zzaragozagildo@gmail.com>
2022-09-04 21:02:07 -0400

    add JS changeable variables

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -10,3 +10,17 @@ const snakeBorder = 'black';
 const foodColor = 'red';
 const unitSize = 25;
 
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+  {x:unitSize *  4, y:0},
+  {x:unitSize *  3, y:0},
+  {x:unitSize *  2, y:0},
+  {x:unitSize, y:0},
+  {x:0, y:0}
+];
+
